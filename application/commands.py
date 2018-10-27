f2e6519b566e16c649b47af303e61b5afd257830
Alex Ariza <alex1995ariza@gmail.com>
2018-10-27 16:59:03 +0200

    added commands send products

--- application/commands.py
+++ application/commands.py
@@ -1,4 +1,6 @@
 # coding=utf-8
+import requests
+from bs4 import BeautifulSoup
 from application import bot
 
 
@@ -7,11 +9,32 @@ def start(message):
     bot.reply_to(message, 'Hello, ' + message.from_user.first_name)
 
 
-@bot.message_handler(func=lambda message: True, content_types=['text'])
-def echo_message(message):
-    """
-    Hace un 'eco' de lo que se recibe y no se ha procesado en algún comando anterior.
-    :param message:
-    :return:
-    """
-    bot.reply_to(message, message.text)
+@bot.message_handler(commands=['search'])
+def search(message):
+	#import ipdb; ipdb.set_trace()
+	if len(message.text.split('/search')[1]) > 1:
+		object_search = message.text.split('/search')[1]
+		url = 'https://www.amazon.es/s/ref=nb_sb_noss_1?__mk_es_ES=ÅMÅŽÕÑ&url=search-alias%3Daps&field-keywords=' + object_search.encode('utf-8')
+		response = requests.get(url)
+		list_response_products = []
+		soup = BeautifulSoup(response.content)
+
+		products = soup.find("ul", {"id": "s-results-list-atf"})
+		list_products = products.findAll("li")
+		for product in list_products:
+			price = product.find("span", {"class": "a-size-base a-color-price s-price a-text-bold"})
+			if hasattr(price, 'text'):
+				price = price.text
+				url = product.find("a", {"a-link-normal a-text-normal"})
+				url = url['href']
+				list_response_products.append({'url':url, 'price':price})
+		html = ''
+		for product_response in list_response_products:
+			html += ' Precio: %s \n Url: %s ' % (product_response['price'], product_response['url'],)
+
+		bot.send_message(message.chat.id, html, parse_mode="HTML")
+	else:
+		bot.send_message(message.chat.id, 'Por favor, escribe /search Producto', parse_mode="HTML")
+
+		#bot.reply_to(message, "Precio = " + product_response['price'])
+		#bot.reply_to(message, product_response['url'])
