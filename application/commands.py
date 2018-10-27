cdcded314eeb042436cf6c88f6f3752929fa19bf
josefrnandezz <i62fealj@uco.es>
2018-10-27 15:15:35 +0200

    departments alright

--- application/commands.py
+++ application/commands.py
@@ -12,17 +12,12 @@ def start(message):
 
 @bot.message_handler(commands=['departments'])
 def departments(message):
-    markup = InlineKeyboardMarkup()
-    markup.row_width = 2
     response = requests.get("https://www.amazon.es/gp/site-directory?ref=nav_shopall_btn", allow_redirects = True)
     soup = BeautifulSoup(response.content)
     categories = soup.findAll("h2",{"class":"popover-category-name"})
+    click_kb = InlineKeyboardMarkup()
     for category in categories:
         name_category = category.get_text()
-        callback_name = name_category
-        markup.add(InlineKeyboardButton(name_category, callback_data=f"cb_yes"),)
-    return markup
-
-@bot.callback_query_handler(func=lambda call: True)
-def callback_query(call):
-    pass
+        click_button = InlineKeyboardButton(name_category, callback_data='clicked')
+        click_kb.row(click_button)
+    bot.send_message(message.chat.id, "<b>Hey friend...</b>", parse_mode="HTML", reply_markup=click_kb, disable_web_page_preview=True)
